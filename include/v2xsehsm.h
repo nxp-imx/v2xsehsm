93954a686bde50d164a9a7f937a218139dc00056
Michael Whitfield <michael.whitfield@nxp.com>
2019-07-29 12:51:36 +0200

    Take version nums from cmake

--- include/v2xsehsm.h
+++ include/v2xsehsm.h
@@ -16,17 +16,7 @@
 
 #include "v2xSe.h"
 #include "hsmstub.h"
-
-#define VERSION_GENERATION	3
-#define VERSION_MAJOR		1
-#define VERSION_MINOR		0
-
-/* Length V2XSE_PLATFORM_IDENTITY = 16 bytes */
-#define PLATFORMINFO_STRING "HSM0IMX800000001"
-
-/* Length V2XSE_SERIAL_NUMBER = 24 bytes */
-#define SERIALNUM_BYTES 	\
-	{0,1,2,3,4,5,6,7,8,9,0,1,2,3,4,5,6,7,8,9,0,1,2,3}
+#include "version.h"
 
 #define V2XSE_STATE_INIT	0
 #define V2XSE_STATE_CONNECTED	1
